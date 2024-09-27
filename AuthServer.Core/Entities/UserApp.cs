374dc5878663329d87f672a3025b807d4f771ad0
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-09-27 16:11:07 +0300

    created entities and IGenericRepo in Core, created Dtos in Shared Layer

---
+++ AuthServer.Core/Entities/UserApp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Entitiy
+{
+    public class UserApp : IdentityUser
+    {
+        //other properties will come default
+        public string City { get; set; }
+    }
+}
