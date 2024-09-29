00c52b581c7f94f26f4a7b4e40f6765459dc25f5
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-09-29 21:49:56 +0300

    Generic Repository and UnitOfWork completed

---
+++ AuthServer.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using AuthServer.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _dbContext = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if(entity != null)
+            {
+                //entity memory de takip edilmesin
+                _dbContext.Entry(entity).State = EntityState.Detached;
+            }
+            return entity;
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public T Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            return entity;
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
+}
