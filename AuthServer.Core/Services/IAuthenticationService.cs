ec54f38738f426780e8b7835453c90b72b48edab
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-09-29 14:43:11 +0300

    IAuthanticationService completed

---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.SharedLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<Response<NoContentDto>> RevokeRefreshToken(string refreshToken); //refreshi kır
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
