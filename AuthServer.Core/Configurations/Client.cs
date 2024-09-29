f101b843bd893b7b057c04b574b454638b1bc464
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-09-29 14:32:19 +0300

    ITokenService and Client entity created

---
+++ AuthServer.Core/Configurations/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configurations
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+
+        //hangi app hangi api ye istek atablilir belirlemek icin
+        public List<String> Audiences { get; set; } 
+    }
+}
