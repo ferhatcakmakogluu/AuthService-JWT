59752631d8bbf8b02e24a185d42f46aa460af3b7
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-02 22:54:13 +0300

    AuthController completed

---
+++ AuthServer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,17 @@
+﻿using AuthServer.SharedLibrary.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult ActionResultInstance<T>(Response<T> response) where T : class 
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
