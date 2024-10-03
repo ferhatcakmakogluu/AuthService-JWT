7bf722238e5aacca4774fd82b0c4561b33616e30
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-03 18:59:45 +0300

    ProductController completed

---
+++ AuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Entitiy;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IService<Product,ProductDto> _productService;
+
+        public ProductController(IService<Product, ProductDto> productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            return ActionResultInstance(await _productService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Update(productDto, productDto.Id));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveProduct(int id)
+        {
+            return ActionResultInstance(await _productService.Remove(id));
+        }
+    }
+}
