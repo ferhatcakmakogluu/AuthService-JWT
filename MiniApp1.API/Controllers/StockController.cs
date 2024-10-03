8aac97225b7e9a38aec9f083090bbf2dbefee72a
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-03 20:18:30 +0300

    Miniapi demo controller functions completed

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MiniApp1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name;
+
+            //username degilde id almak istesek
+            //service kısmında token olusturuken, id yi ClaimTypes.NameIdentifier 'e yazdık
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            var userEmail = User.Claims.FirstOrDefault(x=>x.Type == JwtRegisteredClaimNames.Email);
+
+            return Ok($"Stock -> UserName: {userName} - UserId: {userId} - UserEmail: {userEmail}");
+
+        }
+    }
+}
